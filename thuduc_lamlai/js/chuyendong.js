e3ced557945b92cccd6b84a2c71c7cb9246640c7
vtien035 <108845158+vtien035@users.noreply.github.com>
2024-03-26 08:40:59 +0700

    update

---
+++ thuduc_lamlai/js/chuyendong.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $('#backtop').fadeIn();
+        }else{
+            $('#backtop').fadeOut();
+        }
+    });
+    $("#backtop").click(function(){
+        $('html, body').animate({scrollTop:0},1000);
+    });
+});
