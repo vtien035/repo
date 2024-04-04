b78178b8b61ef28a4bca70c77466edcee013a6fc
vtien035 <108845158+vtien035@users.noreply.github.com>
2024-04-04 08:39:51 +0700

    up

---
+++ thuduc_lamlai/js/swiper.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true, // Cho phép lặp lại ảnh
+    autoplay: {
+      delay: 3000, // Thời gian chuyển đổi giữa các ảnh (đơn vị: ms)
+    },
+  });
+  
